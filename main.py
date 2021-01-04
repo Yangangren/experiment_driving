4b9ac5dd875928e24a0b9303c6d4d6b6c1893a76
ryg18 <ryg18@mails.tsinghua.edu.cn>
2021-01-04 13:20:21 +0800

    case0_noise1_20210104_131914

--- main.py
+++ main.py
@@ -71,12 +71,12 @@ def built_parser():
     parser.add_argument('--if_radar', type=bool, default=False)
     task = parser.parse_args().task
     case = parser.parse_args().case
-    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-03-12-38-00'.format(task))
+    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-04-01-15-35'.format(task))
     parser.add_argument('--load_ite', type=str, default=100000)
     parser.add_argument('--noise_factor', type=float, default=1.)
-    parser.add_argument('--surr_flag', type=bool, default=True)
+    parser.add_argument('--surr_flag', type=bool, default=False)
     parser.add_argument('--model_only_test', type=bool, default=False)
-    parser.add_argument('--backup', type=str, default='')
+    parser.add_argument('--backup', type=str, default='ABS_BAD_POLICY')
     noise = int(parser.parse_args().noise_factor)
     result_dir = './record/{task}/case{case}_noise{noise}_{time}'.format(task=task,
                                                                          case=case,
