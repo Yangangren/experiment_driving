1e89f5c1aa6f9bdc63d2eda9b3901f7fde56e215
ryg18 <ryg18@mails.tsinghua.edu.cn>
2021-01-04 17:09:47 +0800

    case0_noise1_20210104_170822

--- main.py
+++ main.py
@@ -71,13 +71,13 @@ def built_parser():
     parser.add_argument('--if_radar', type=bool, default=False)
     task = parser.parse_args().task
     case = parser.parse_args().case
-    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-04-15-50-23'.format(task))
-    parser.add_argument('--load_ite', type=str, default=40000)
+    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-04-16-24-58'.format(task))
+    parser.add_argument('--load_ite', type=str, default=55000)
     parser.add_argument('--noise_factor', type=float, default=1.)
     parser.add_argument('--surr_flag', type=bool, default=True)
     parser.add_argument('--model_only_test', type=bool, default=False)
     parser.add_argument('--backup', type=str, default='abso_POLICY: add_random init:0103_model_parameters CLIP TORQUE TO 250: CANCEL inertia: '
-    'modify and clipped state_gps heading: add surr traffic, debug vehicle dynamics')
+    'modify and add surr traffic, debug vehicle dynamics: add done condition')
     noise = int(parser.parse_args().noise_factor)
     result_dir = './record/{task}/case{case}_noise{noise}_{time}'.format(task=task,
                                                                          case=case,
