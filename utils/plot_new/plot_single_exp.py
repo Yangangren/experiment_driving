63c8076f49bd6bc18f5b7cf6a2e939d88914b7c6
ryg18 <ryg18@mails.tsinghua.edu.cn>
2021-01-04 15:55:58 +0800

    case0_noise1_20210104_154002

--- utils/plot_new/plot_single_exp.py
+++ utils/plot_new/plot_single_exp.py
@@ -123,7 +123,7 @@ def single_plot_other_vehicles(data_all, exp_index, highlight=True):
 
 
 if __name__ == '__main__':
-    exp_index = 'left/case0_noise1_20210104_145511'
+    exp_index = 'left/case0_noise1_20210104_153822'
     data_all, keys_for_data = load_data(exp_index)
     print(keys_for_data)
 
