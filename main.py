f8fc032ab5f48a680c94c0fc428d0ba179dbf453
ryg18 <ryg18@mails.tsinghua.edu.cn>
2021-01-06 17:01:28 +0800

    06_165858_real

--- main.py
+++ main.py
@@ -66,17 +66,17 @@ def plot_agent(shared_list, lock, task, model_only_test):
 def built_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, default='left')
-    parser.add_argument('--case', type=int, default=2)
+    parser.add_argument('--case', type=int, default=0)
     parser.add_argument('--if_save', type=bool, default=True)
     parser.add_argument('--if_radar', type=bool, default=False)
     task = parser.parse_args().task
     case = parser.parse_args().case
-    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-06-00-47-43'.format(task))
+    parser.add_argument('--load_dir', type=str, default='./utils/models/{}/experiment-2021-01-06-14-33-09'.format(task))
     parser.add_argument('--load_ite', type=str, default=150000)
     parser.add_argument('--noise_factor', type=float, default=1.)
     parser.add_argument('--surr_flag', type=bool, default=True)
-    parser.add_argument('--model_only_test', type=bool, default=True)
-    parser.add_argument('--clipped_v', type=float, default=3., help='m/s')
+    parser.add_argument('--model_only_test', type=bool, default=False)
+    parser.add_argument('--clipped_v', type=float, default=300., help='m/s')
 
     parser.add_argument('--backup', type=str, default='CLIP TORQUE 100 v>3, v<3 250: CANCEL inertia;')
 
