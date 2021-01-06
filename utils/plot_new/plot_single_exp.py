9dfd0826d6126c59675b494f32e84086f7ed4354
mahaitongdae <mahaitongdae@foxmail.com>
2021-01-06 22:41:56 +0800

    clarify single plot and bird view plot

--- utils/plot_new/plot_single_exp.py
+++ utils/plot_new/plot_single_exp.py
@@ -4,45 +4,87 @@ from utils.plot_new.plot_utils.search_index import search_geq,search_leq
 import os
 
 
-def single_plot(data_all, keys, **kwargs):
+def single_plot(data_all, keys, path, title, **kwargs):
+    """
+
+    :param data_all:
+    :param keys: list elements is key or tuple of 2 keys
+                    key is for plots whose x-axis is time
+                    tuples of keys is for plots whose x-axis tuple[0], y-axis is tuple[1]
+    :param path:
+    :param kwargs: ['x_lim','y_lim']
+    :return:
+    """
+
+    exp_index = path[0]
+    model_index = path[1]
+
     if 'fig_num' in kwargs.keys():
         plt.figure(kwargs['fig_num'])
     else:
         plt.figure(dpi=200)
 
-    if kwargs['highlight'] == True:
-        min_index = data_all['VehicleMode'].index(1.0)
-        max_index = min_index + data_all['VehicleMode'].count(1.0)
-        if max_index >= len(data_all['VehicleMode']):
-            max_index = -1
+    task = model_index.split('/')[0]
+    model_or_real = exp_index.split('_')[-1]
 
+    # ----------- plot ---------------
 
     labels = []
     for key in keys:
         if isinstance(key, tuple):
-            plt.plot(data_all[key[0]], data_all[key[1]])
-            labels.append(key[1])
+            try:
+                plt.plot(data_all[key[0]], data_all[key[1]])
+                labels.append(key[1])
+            except (KeyError):
+                print('No key {} in record!'.format(key))
         else:
-            plt.plot(data_all['Time'], data_all[key])
+            try:
+                plt.plot(data_all['Time'], data_all[key])
+            except (KeyError):
+                print('No key {} in record!'.format(key))
 
     if isinstance(keys[0], tuple):
         plt.xlabel(keys[0][0])
     else:
         plt.xlabel('time /s')
         labels = keys
-        model_or_real = kwargs['path'][0].split('_')[-1]
-        if model_or_real == 'model':
-            pass
-        else:
-            axes = plt.gca()
-            ylim = axes.get_ylim()
-            plt.plot([data_all['Time'][min_index], data_all['Time'][min_index]], ylim, c='red', linestyle='--')
-            plt.plot([data_all['Time'][max_index], data_all['Time'][max_index]], ylim, c='red', linestyle='--')
-            in_index, _ = search_geq(data_all['GaussY'], -14.0)
-            plt.plot([data_all['Time'][in_index], data_all['Time'][in_index]], ylim, c='coral', linestyle='--')
+        if model_or_real == 'real':
 
-            out_index, _ = search_leq(data_all['GaussX'], -11.0)
-            plt.plot([data_all['Time'][out_index], data_all['Time'][out_index]], ylim, c='coral', linestyle='--')
+            # search autonomous driving zone
+
+            try:
+
+                min_index = data_all['VehicleMode'].index(1.0)
+                max_index = min_index + data_all['VehicleMode'].count(1.0)
+                if max_index >= len(data_all['VehicleMode']):
+                    max_index = -1
+
+                axes = plt.gca()
+                ylim = axes.get_ylim()
+                plt.plot([data_all['Time'][min_index], data_all['Time'][min_index]], ylim, c='red', linestyle='--')
+                plt.plot([data_all['Time'][max_index], data_all['Time'][max_index]], ylim, c='red', linestyle='--')
+
+            except:
+                pass
+
+            try:
+                in_index, _ = search_geq(data_all['GaussY'], -14.0)
+                plt.plot([data_all['Time'][in_index], data_all['Time'][in_index]], ylim, c='coral', linestyle='--')
+            except:
+                pass
+                # print("Not enter the intersection!")
+
+            try:
+                if task == 'left':
+                    out_index, _ = search_leq(data_all['GaussX'], -11.0)
+                elif task == 'straight':
+                    out_index, _ = search_geq(data_all['GaussY'], 11.0)
+                elif task == 'right':
+                    out_index, _ = search_geq(data_all['GaussX'], 11.0)
+                plt.plot([data_all['Time'][out_index], data_all['Time'][out_index]], ylim, c='coral', linestyle='--')
+            except:
+                pass
+                # print('Not leave the intersection!')
 
 
     plt.legend(labels=labels, loc='best')
@@ -52,99 +94,121 @@ def single_plot(data_all, keys, **kwargs):
         plt.xlim(kwargs['x_lim'])
     if 'y_lim' in kwargs.keys():
         plt.ylim(kwargs['y_lim'])
-    if 'title' in kwargs.keys():
-        plt.title(kwargs['title'])
-    if 'path' in kwargs.keys():
-        model_dir = kwargs['path'][1]
-        proj_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        fig_path = proj_root_dir + '/utils/models/'+ model_dir + '/record/' + kwargs['path'][0] + '/figure/'
-        data_fig_path = proj_root_dir + '/utils/models/'+ model_dir + '/record/' + kwargs['path'][0] + '/figure/data_fig/'
-        if not os.path.exists(fig_path):
-            os.mkdir(fig_path)
-            os.mkdir(data_fig_path)
-        name = data_fig_path + kwargs['title'] +'.jpg'
-        plt.savefig(name)
-    else:
-        plt.show()
 
-def single_plot_time_series(data_all, path, AutoMode = True):
-    highlight = AutoMode
+    plt.title(title)
+
+    proj_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    fig_path = proj_root_dir + '/utils/models/'+ model_index + '/record/' + exp_index + '/figure/'
+    data_fig_path = fig_path + 'data_fig/'
+    if not os.path.exists(fig_path):
+        os.mkdir(fig_path)
+        os.mkdir(data_fig_path)
+    name = data_fig_path + title +'.jpg'
+    plt.savefig(name)
+
+
+def single_plot_time_series(data_all, path,):
     single_plot(data_all, ['SteerAngleAct', 'SteerAngleAim'],
-                title='Steering-Act', path=path, highlight=highlight)
+                path=path, title='Steering-Act')
     single_plot(data_all, ['front_wheel_deg'],
-                title='Steering-Front wheel deg', path=path, highlight=highlight)
+                path=path, title='Steering-Front wheel deg')
     single_plot(data_all, ['accActual', 'a_x'],
-                title='Deceleration-acc', path=path, highlight=highlight)
+                path=path, title='Deceleration-acc')
     single_plot(data_all, ['GaussX', 'GaussY'],
-                title='State-XY', path=path, highlight=highlight)
+                path=path, title='State-XY')
     single_plot(data_all, ['ego_vx', 'ego_vy'],
-                title='Obs-ego velocity', path=path, highlight=highlight)
+                path=path, title='Obs-ego velocity')
     single_plot(data_all, ['ego_x'],
-                title='Obs-ego position', path=path, highlight=highlight)
+                path=path, title='Obs-ego position')
     single_plot(data_all, ['ego_phi'],
-                title='Obs-ego phi', path=path, highlight=highlight)
+                path=path, title='Obs-ego phi')
     single_plot(data_all, ['tracking_delta_y'],
-                title='Obs-ego delta y', path=path, highlight=highlight)
+                path=path, title='Obs-ego delta y')
     single_plot(data_all, ['tracking_delta_phi'],
-                title='Obs-ego delta phi', path=path, highlight=highlight)
+                path=path, title='Obs-ego delta phi')
     single_plot(data_all, ['tracking_delta_v'],
-                title='Obs-ego delta v', path=path, highlight=highlight)
+                path=path, title='Obs-ego delta v')
     single_plot(data_all, ['Heading'],
-                title='State-Heading', path=path, highlight=highlight)
+                path=path, title='State-Heading')
     single_plot(data_all, ['accActual'],
-                title='Deceleration-Acc actual', path=path, highlight=highlight)
+                path=path, title='Deceleration-Acc actual')
     single_plot(data_all, ['NorthVelocity', 'EastVelocity'],
-                title='Velocity-NEvelo', path=path, highlight=highlight)
+                path=path, title='Velocity-NEvelo')
     single_plot(data_all, ['VehicleMode'],
-                title='Mode', path=path, highlight=highlight)
+                path=path, title='Mode')
     single_plot(data_all, ['GpsSpeed', 'VehicleSPeedAct'],
-                title='State-Speed', path=path, highlight=highlight)
+                path=path, title='State-Speed')
     single_plot(data_all, ['YawRate'],
-                title='State-Yaw rate', path=path, highlight=highlight)
+                path=path, title='State-Yaw rate')
     single_plot(data_all, ['time_receive_gps','time_receive_can', 'time_receive_radar','time_decision'],
-                title='Time',y_lim=[0,0.2], path=path, highlight=highlight)
+                path=path, title='Time',y_lim=[0,0.2])
 
-def single_plot_compare_response(data_all, path, AutoMode=True):
-    highlight = AutoMode
+def single_plot_compare_response(data_all, path):
+    """
+    for response compare with real vehicle and model.
+
+    :param data_all:
+    :param path:
+    :return:
+    """
     single_plot(data_all, ['GaussX','model_x_in_real_action', 'model_x_in_model_action'],
-                title='Response-positionX', path=path, highlight=highlight)
+                path=path, title='Response-positionX')
     single_plot(data_all, ['GaussY', 'model_y_in_real_action', 'model_y_in_model_action'],
-                title='Response-positionY', path=path, highlight=highlight)
+                path=path, title='Response-positionY')
     single_plot(data_all, ['Heading', 'model_phi_in_real_action', 'model_phi_in_model_action'],
-                title='Response-heading', path=path, highlight=highlight)
+                path=path, title='Response-heading')
     single_plot(data_all, ['ego_vx', 'model_vx_in_real_action', 'model_vx_in_model_action'],
-                title='Response-speedX', path=path, highlight=highlight)
+                path=path, title='Response-speedX')
     single_plot(data_all, ['ego_vy', 'model_vy_in_real_action', 'model_vy_in_model_action'],
-                title='Response-speedY', path=path, highlight=highlight)
+                path=path, title='Response-speedY')
     single_plot(data_all, ['YawRate', 'model_r_in_real_action', 'model_r_in_model_action'],
-                title='Response-yawrate', path=path, highlight=highlight)
+                path=path, title='Response-yawrate')
     single_plot(data_all, ['model_front_wheel_rad_in_real_action', 'model_front_wheel_rad_in_model_action'],
-                title='Response-steering', path=path, highlight=highlight)
+                path=path, title='Response-steering')
     single_plot(data_all, ['accActual','Deceleration', 'model_acc_in_real_action', 'model_acc_in_model_action'],
-                title='Response-acc', path=path, highlight=highlight)
-
-
+                path=path, title='Response-acc')
 
 
 def single_plot_other_series(data_all, path):
-    single_plot(data_all, [('GaussX', 'GaussY'),('model_x_in_model_action','model_y_in_model_action'),('model_x_in_real_action','model_y_in_real_action')], title='Trajectory', path=path,  highlight=False)
+    """
+    for plots whose x-axis is not time, for example, trajectory.
+    :param data_all:
+    :param path:
+    :return:
+    """
+    single_plot(data_all,
+                [('GaussX', 'GaussY'),
+                 ('model_x_in_model_action','model_y_in_model_action'),
+                 ('model_x_in_real_action','model_y_in_real_action')],
+                 path=path, title='Trajectory')
 
-def single_plot_other_vehicles(data_all, path, highlight=True):
-    for i in range(6):
-        for real_key in ['delta_x','delta_y','delta_v','delta_phi']:
+
+def single_plot_obs_other_vehicles(data_all, path, others_num = 6):
+    """
+    plot other vehicle in state others.
+    :param data_all:
+    :param path:
+    :param others_num:
+    :return:
+    """
+    for i in range(others_num):
+        for real_key in ['x','y','v','phi']:
             key = 'other' + str(i) + '_' + real_key
             title = 'Other_obs-' + key
-            single_plot(data_all, [key], title=title,  path=path, highlight=highlight)
+            single_plot(data_all, [key],  path=path, title=title)
 
 
 if __name__ == '__main__':
-    exp_index = 'case0/noise1/06_195738_real'
-    model_index = 'right/experiment-2021-01-05-01-05-43'
+    exp_index = 'case0/noise1/06_162838_real'
+    model_index = 'left/experiment-2021-01-06-00-47-43'
     data_all, keys_for_data = load_data(model_index, exp_index)
     print(keys_for_data)
     path = (exp_index, model_index)
 
-    single_plot_time_series(data_all, path, AutoMode=True) # if not switch into auto mode, add kwargs: VehicleMode=False
+    # plots whose x-axis is time
+    single_plot_time_series(data_all, path) # if not switch into auto mode, add kwargs: VehicleMode=False
+    # plots whose x-axis is not time, for example trajectory
     single_plot_other_series(data_all, path)
-    # single_plot_other_vehicles(data_all, exp_index, highlight=False)
-    single_plot_compare_response(data_all, path, AutoMode=True)
+    # for convenience
+    single_plot_obs_other_vehicles(data_all, path)
+    single_plot_compare_response(data_all, path)
