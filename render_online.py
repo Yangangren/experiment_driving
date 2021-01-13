309a4e3d0c54bc6899f4f46698005643fc0c50e9
mht <mahaitongdae@foxmail.com>
2021-01-13 13:13:52 +0800

    add static rendering

--- render_online.py
+++ render_online.py
@@ -10,8 +10,35 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from PIL.Image import open
-import cv2
+import xml.dom.minidom
+EGO_LENGTH = 4.8
+EGO_WIDTH = 2.0
+STATE_OTHER_LENGTH = EGO_LENGTH
+STATE_OTHER_WIDTH = EGO_WIDTH
 
+def rotate_coordination(orig_x, orig_y, orig_d, coordi_rotate_d):
+    """
+    :param orig_x: original x
+    :param orig_y: original y
+    :param orig_d: original degree
+    :param coordi_rotate_d: coordination rotation d, positive if anti-clockwise, unit: deg
+    :return:
+    transformed_x, transformed_y, transformed_d(range:(-180 deg, 180 deg])
+    """
+
+    coordi_rotate_d_in_rad = coordi_rotate_d * math.pi / 180
+    transformed_x = orig_x * math.cos(coordi_rotate_d_in_rad) + orig_y * math.sin(coordi_rotate_d_in_rad)
+    transformed_y = -orig_x * math.sin(coordi_rotate_d_in_rad) + orig_y * math.cos(coordi_rotate_d_in_rad)
+    transformed_d = orig_d - coordi_rotate_d
+    if transformed_d > 180:
+        while transformed_d > 180:
+            transformed_d = transformed_d - 360
+    elif transformed_d <= -180:
+        while transformed_d <= -180:
+            transformed_d = transformed_d + 360
+    else:
+        transformed_d = transformed_d
+    return transformed_x, transformed_y, transformed_d
 
 class Render():
     def __init__(self, shared_list, lock, task, model_only_test=False):
@@ -21,124 +48,323 @@ class Render():
         self.model_only_test = model_only_test
         self.step_old = -1
         self.acc_timer = TimerStat()
+        self._load_xml()
+        self.red_img = self._read_png('utils/Rendering/red.png')
+        self.green_img = self._read_png('utils/Rendering/green.png')
+        self.GL_TEXTURE_RED = glGenTextures(1)
+        self.GL_TEXTURE_GREEN = glGenTextures(1)
         self._opengl_init()
 
+    def _read_png(self, path):
+        im = open(path)
+        ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGBA", 0, -1)
+        return ix, iy, image
+
     def _opengl_init(self):
-        glutInit()  # 1. 初始化glut库
+        glutInit()
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH)
-
         glutInitContextProfile(GLUT_CORE_PROFILE)
-        glutInitWindowSize(600, 600)
-        glutInitWindowPosition(0, 0)
-        glutCreateWindow('Quidam Of OpenGL')  # 2. 创建glut窗口
-        glutDisplayFunc(self.test_texture)  # 3. 注册回调函数draw()
-        # glutTimerFunc(500, self.test_draw_2, None)
-        glutMainLoop()  # 4. 进入glut主循环
+        glutInitWindowSize(1000, 1000)
+        glutInitWindowPosition(460, 0)
+        glutCreateWindow('Crossroad')
+        glutDisplayFunc(self.render_map)
+        glutMainLoop()
 
-    def test_draw(self):
+    def _load_xml(self, path="./utils/sumo_files/a.net.xml"):
+        dom_obj = xml.dom.minidom.parse(path)
+        element_obj = dom_obj.documentElement
+        self.sub_element_edge = element_obj.getElementsByTagName("edge")
+        self.sub_element_junction = element_obj.getElementsByTagName("junction")
+        self.sub_element_tlLogic = element_obj.getElementsByTagName("tlLogic")
+
+    def _draw_map(self, scale):
+        sub_element_edge = self.sub_element_edge
+        sub_element_junction = self.sub_element_junction
+
+        def bit_pattern(*args):
+            args = list(args[:16])
+            args = args + [0] * (16 - len(args))
+            base = 0
+            for arg in args:
+                base = base << 1
+                base += bool(arg)
+            return base
+
+        for i in range(len(sub_element_edge)):
+            if sub_element_edge[i].getAttribute('function') != 'internal':
+                sub_element_lane = sub_element_edge[i].getElementsByTagName("lane")
+                for j in range(len(sub_element_lane)):
+                    shape = sub_element_lane[j].getAttribute("shape")
+                    type = str(sub_element_lane[j].getAttribute("allow"))
+                    try:
+                        width = float(sub_element_lane[j].getAttribute("width"))
+                    except:
+                        width = 3.5
+                    shape_list = shape.split(" ")
+                    for k in range(len(shape_list) - 1):
+                        shape_point_1 = shape_list[k].split(",")
+                        shape_point_2 = shape_list[k + 1].split(",")
+                        shape_point_1[0] = float(shape_point_1[0])
+                        shape_point_1[1] = float(shape_point_1[1])
+                        shape_point_2[0] = float(shape_point_2[0])
+                        shape_point_2[1] = float(shape_point_2[1])
+                        # 道路顶点生成
+                        dx1 = shape_point_2[0] - shape_point_1[0]
+                        dy1 = shape_point_2[1] - shape_point_1[1]
+                        v1 = np.array([-dy1, dx1])
+                        absdx = abs(shape_point_1[0] - shape_point_2[0])
+                        absdy = abs(shape_point_1[1] - shape_point_2[1])
+                        if math.sqrt(absdx * absdx + absdy * absdy) > 0:
+                            v3 = v1 / math.sqrt(absdx * absdx + absdy * absdy) * width * 0.5
+                            [x1, y1] = ([shape_point_1[0], shape_point_1[1]] + v3) / scale
+                            [x2, y2] = ([shape_point_1[0], shape_point_1[1]] - v3) / scale
+                            [x4, y4] = ([shape_point_2[0], shape_point_2[1]] + v3) / scale
+                            [x3, y3] = ([shape_point_2[0], shape_point_2[1]] - v3) / scale  # 0.0176 * v3
+                            glBegin(GL_POLYGON)  # 开始绘制单车道
+                            if type == 'pedestrian':
+                                glColor3f(0.663, 0.663, 0.663)
+                            elif type == 'bicycle':
+                                glColor3f(0.455, 0.721, 0.926)
+                            else:
+                                glColor3f(0.0, 0.0, 0.0)
+                            glVertex2f(x1, y1)
+                            glVertex2f(x2, y2)
+                            glVertex2f(x3, y3)
+                            glVertex2f(x4, y4)
+                            glEnd()
+
+                            glLineWidth(1.0)
+                            glLineStipple(3, bit_pattern(
+                                0, 0, 0, 0,
+                                0, 0, 0, 0,
+                                1, 1, 1, 1,
+                                1, 1, 1, 1,
+                            ))
+                            glEnable(GL_LINE_STIPPLE)
+                            glBegin(GL_LINES)
+                            glColor3f(1.0, 1.0, 1.0)
+                            glVertex2f(x2, y2)
+                            glVertex2f(x3, y3)
+                            glVertex2f(x1, y1)
+                            glVertex2f(x4, y4)
+                            glEnd()
+                            glDisable(GL_LINE_STIPPLE)
+                            glLineWidth(10.0)
+                            glBegin(GL_LINES)
+                            glColor3f(1.0, 1.0, 1.0)
+                            glVertex2f(x3, y3)
+                            glVertex2f(x4, y4)
+                            glEnd()
+                            glLineWidth(2.0)
+                            glBegin(GL_LINES)
+                            glColor3f(0.8275, 0.8275, 0.8275)
+                            if j == sub_element_lane.length - 1:
+                                glVertex2f(x1, y1)
+                                glVertex2f(x4, y4)
+                            if j == 0:
+                                glVertex2f(x2, y2)
+                                glVertex2f(x3, y3)
+                            glEnd()
+
+        for i in range(len(sub_element_junction)):
+            shape = sub_element_junction[i].getAttribute("shape")
+            shape_list = shape.split(" ")
+
+            glLineWidth(1)
+            glBegin(GL_POLYGON)
+            glColor3f(0.0, 0.0, 0.0)
+            for k in range(len(shape_list)):
+                shape_point = shape_list[k].split(",")
+                if shape_point[0] != '':
+                    glVertex2f((float(shape_point[0]) / scale) * 1, (float(shape_point[1]) / scale) * 1)
+            glEnd()
+
+
+    def _draw_zebra(self, loc, width, length, scale, shape, single_height=0.8):
+        glLineWidth(1)
+        glColor3f(0.8275, 0.8275, 0.8275)
+        if shape == 'vertical':
+            for i in range(int(length/single_height)):
+                glBegin(GL_POLYGON)
+                x1, y1 = (loc - width / 2) / scale, (2 * i * single_height) / scale
+                x2, y2 = (loc + width / 2) / scale, (2 * i * single_height) / scale
+                x3, y3 = (loc + width / 2) / scale, ((2 * i + 1) * single_height) / scale
+                x4, y4 = (loc - width / 2) / scale, ((2 * i + 1) * single_height) / scale
+                glVertex2f(x1, y1)
+                glVertex2f(x2, y2)
+                glVertex2f(x3, y3)
+                glVertex2f(x4, y4)
+                glEnd()
+                glBegin(GL_POLYGON)
+                x1, y1 = (loc - width / 2) / scale, -((2 * i + 2) * single_height) / scale
+                x2, y2 = (loc + width / 2) / scale, -((2 * i + 2) * single_height) / scale
+                x3, y3 = (loc + width / 2) / scale, -((2 * i + 1) * single_height) / scale
+                x4, y4 = (loc - width / 2) / scale, -((2 * i + 1) * single_height) / scale
+                glVertex2f(x1, y1)
+                glVertex2f(x2, y2)
+                glVertex2f(x3, y3)
+                glVertex2f(x4, y4)
+                glEnd()
+        elif shape == 'horizontal':
+            for i in range(int(length/single_height)):
+                glBegin(GL_POLYGON)
+                y1, x1 = (loc - width / 2) / scale, (2 * i * single_height) / scale
+                y2, x2 = (loc + width / 2) / scale, (2 * i * single_height) / scale
+                y3, x3 = (loc + width / 2) / scale, ((2 * i + 1) * single_height) / scale
+                y4, x4 = (loc - width / 2) / scale, ((2 * i + 1) * single_height) / scale
+                glVertex2f(x1, y1)
+                glVertex2f(x2, y2)
+                glVertex2f(x3, y3)
+                glVertex2f(x4, y4)
+                glEnd()
+                glBegin(GL_POLYGON)
+                y1, x1 = (loc - width / 2) / scale, -((2 * i + 2) * single_height) / scale
+                y2, x2 = (loc + width / 2) / scale, -((2 * i + 2) * single_height) / scale
+                y3, x3 = (loc + width / 2) / scale, -((2 * i + 1) * single_height) / scale
+                y4, x4 = (loc - width / 2) / scale, -((2 * i + 1) * single_height) / scale
+                glVertex2f(x1, y1)
+                glVertex2f(x2, y2)
+                glVertex2f(x3, y3)
+                glVertex2f(x4, y4)
+                glEnd()
+
+    def render_map(self, real_x=0, real_y=0, scale=50):
+        LOC_X = -real_x / scale
+        LOC_Y = -real_y / scale
+        glClearColor(0.1333, 0.545, 0.1333, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # ---------------------------------------------------------------
-        glBegin(GL_LINES)  # 开始绘制线段（世界坐标系）
-
-        # 以红色绘制x轴
-        glColor4f(1.0, 0.0, 0.0, 1.0)  # 设置当前颜色为红色不透明
-        glVertex3f(-0.8, 0.0, 0.0)  # 设置x轴顶点（x轴负方向）
-        glVertex3f(0.8, 0.0, 0.0)  # 设置x轴顶点（x轴正方向）
-
-        # 以绿色绘制y轴
-        glColor4f(0.0, 1.0, 0.0, 1.0)  # 设置当前颜色为绿色不透明
-        glVertex3f(0.0, -0.8, 0.0)  # 设置y轴顶点（y轴负方向）
-        glVertex3f(0.0, 0.8, 0.0)  # 设置y轴顶点（y轴正方向）
-
-        # 以蓝色绘制z轴
-        glColor4f(0.0, 0.0, 1.0, 1.0)  # 设置当前颜色为蓝色不透明
-        glVertex3f(0.0, 0.0, -0.8)  # 设置z轴顶点（z轴负方向）
-        glVertex3f(0.0, 0.0, 0.8)  # 设置z轴顶点（z轴正方向）
-
-        glEnd()  # 结束绘制线段
-
-        # ---------------------------------------------------------------
-        glBegin(GL_TRIANGLES)  # 开始绘制三角形（z轴负半区）
-
-        glColor4f(1.0, 0.0, 0.0, 0.5)  # 设置当前颜色为红色不透明
-        glVertex3f(-0.5, -0.366, -0.5)  # 设置三角形顶点
-        glColor4f(0.0, 1.0, 0.0, 0.5)  # 设置当前颜色为绿色不透明
-        glVertex3f(0.5, -0.366, -0.5)  # 设置三角形顶点
-        glColor4f(0.0, 0.0, 1.0, 0.5)  # 设置当前颜色为蓝色不透明
-        glVertex3f(0.0, 0.5, -0.5)  # 设置三角形顶点
-
-        glEnd()  # 结束绘制三角形
-
-        # ---------------------------------------------------------------
-        glFlush()  # 清空缓冲区，将指令送往硬件立即执行
-
-    def test_texture(self):
-        # glShadeModel(GL_SMOOTH)  # 平滑着色
-        glEnable(GL_DEPTH_TEST)  # 深度测试
-        # glDepthMask(GL_FALSE)
-        # glEnable(GL_CULL_FACE)  # 只渲染某一面
-        # glFrontFace(GL_CCW)  # 逆时针正面
-        glEnable(GL_ALPHA_TEST)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glTranslatef(LOC_X, LOC_Y, 0)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glEnable(GL_TEXTURE_2D)  # 启用2D纹理映射
-        glAlphaFunc(GL_GREATER, 0.9)
-        # 载入纹理图像：
-        # ReadImage()
-        im = open('./utils/Rendering/100.png')
-        # im = im.convert('RGBA')
-        # r, g, b, alpha = im.split()
-        # alpha = alpha.point(lambda i: i > 0 and 80)
-        # im.putalpha(alpha)
-        ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGBA", 0, -1)
+        glEnable(GL_POLYGON_SMOOTH)
+        glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST)
 
-        # 生成纹理对象：
-        ID = glGenTextures(1)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self._draw_map(scale)
+        self._draw_zebra(-17, 6, 7, scale, 'vertical')
+        self._draw_zebra(17, 6, 7, scale, 'vertical')
+        self._draw_zebra(26, 6, 4, scale, 'horizontal')
+        self._draw_zebra(-22, 6, 4, scale, 'horizontal')
 
-        glBindTexture(GL_TEXTURE_2D, ID)  # 绑定纹理：
 
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)  # 支持4字节对齐
+        def draw_rotate_rec(x, y, a, l, w, scale, color='o'):
+            RU_x, RU_y, _ = rotate_coordination(l / 2, w / 2, 0, -a)
+            RD_x, RD_y, _ = rotate_coordination(l / 2, -w / 2, 0, -a)
+            LU_x, LU_y, _ = rotate_coordination(-l / 2, w / 2, 0, -a)
+            LD_x, LD_y, _ = rotate_coordination(-l / 2, -w / 2, 0, -a)
+            glBegin(GL_POLYGON)
+            if color == 'o':
+                glColor3f(1.0, 0.647, 0.0)
+            glVertex2f((RU_x + x) / scale, (RU_y + y) / scale)
+            glVertex2f((RD_x + x) / scale, (RD_y + y) / scale)
+            glVertex2f((LD_x + x) / scale, (LD_y + y) / scale)
+            glVertex2f((LU_x + x) / scale, (LU_y + y) / scale)
+            glEnd()
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)  # S方向上贴图
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)  # T方向上贴图
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)  # 放大纹理过滤方式
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)  # 缩小纹理过滤方式
+        # state_other = self.shared_list[4].copy()
+        state_other = self.shared_list.copy()
+        # plot cars
+        for veh in state_other:
+            veh_x = veh['x']
+            veh_y = veh['y']
+            veh_phi = veh['phi']
+            veh_l = STATE_OTHER_LENGTH
+            veh_w = STATE_OTHER_WIDTH
+            # plot_phi_line(veh_x, veh_y, veh_phi, 'black')
+            draw_rotate_rec(veh_x, veh_y, veh_phi, veh_l, veh_w, scale)
 
-        glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)  # 载入纹理：
+        v_light = 0
+        if v_light == 0:
+            self._texture_light(self.green_img, (-8, 20), 'U', scale)
+            self._texture_light(self.green_img, (0, -18), 'D', scale)
+            self._texture_light(self.red_img, (-14, -12), 'L', scale)
+            self._texture_light(self.red_img, (11, 4), 'R', scale)
+        elif v_light != 0:
+            self._texture_light(self.red_img, (-8, 20), 'U', scale)
+            self._texture_light(self.red_img, (0, -18), 'D', scale)
+            self._texture_light(self.green_img, (-14, -12), 'L', scale)
+            self._texture_light(self.green_img, (11, 4), 'R', scale)
 
-        glMatrixMode(GL_MODELVIEW)  # 选择模型观察矩阵
-        glLoadIdentity()  # 重置模型观察矩阵
-        glMatrixMode(GL_PROJECTION)  # 选择投影矩阵
+        glutSwapBuffers()
+
+        glDisable(GL_BLEND)
+        glDisable(GL_LINE_SMOOTH)
+        glDisable(GL_POLYGON_SMOOTH)
+
+    def _texture_light(self, img, loc, edge, scale, size=(8, 3)):
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, self.GL_TEXTURE_RED)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, img[0], img[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, img[2])
+
+        glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        if edge == 'U' or edge=='D':
+            x1, y1 = loc[0] / scale, loc[1] / scale
+            x2, y2 = (loc[0] + size[0]) / scale, loc[1] / scale
+            x3, y3 = loc[0] / scale, (loc[1] + size[1]) / scale
+            x4, y4 = (loc[0] + size[0]) / scale, (loc[1] + size[1]) / scale
+        elif edge == 'L' or edge=='R':
+            x1, y1 = loc[0] / scale, loc[1] / scale
+            x2, y2 = (loc[0] + size[1]) / scale, loc[1] / scale
+            x3, y3 = loc[0] / scale, (loc[1] + size[0]) / scale
+            x4, y4 = (loc[0] + size[1]) / scale, (loc[1] + size[0]) / scale
+        glEnable(GL_TEXTURE_2D)
+        glBegin(GL_QUADS)
+        if edge == 'U':
+            glTexCoord2f(0.0, 0.0)
+            glVertex3f(x4, y4, 0.0)
+            glTexCoord2f(1.0, 0.0)
+            glVertex3f(x3, y3, 0.0)
+            glTexCoord2f(1.0, 1.0)
+            glVertex3f(x1, y1, 0.0)
+            glTexCoord2f(0.0, 1.0)
+            glVertex3f(x2, y2, 0.0)
+        elif edge == 'D':
+            glTexCoord2f(0.0, 0.0)
+            glVertex3f(x1, y1, 0.0)
+            glTexCoord2f(1.0, 0.0)
+            glVertex3f(x2, y2, 0.0)
+            glTexCoord2f(1.0, 1.0)
+            glVertex3f(x4, y4, 0.0)
+            glTexCoord2f(0.0, 1.0)
+            glVertex3f(x3, y3, 0.0)
+        elif edge == 'L':
+            glTexCoord2f(0.0, 0.0)
+            glVertex3f(x3, y3, 0.0)
+            glTexCoord2f(1.0, 0.0)
+            glVertex3f(x1, y1, 0.0)
+            glTexCoord2f(1.0, 1.0)
+            glVertex3f(x2, y2, 0.0)
+            glTexCoord2f(0.0, 1.0)
+            glVertex3f(x4, y4, 0.0)
+        elif edge == 'R':
+            glTexCoord2f(0.0, 0.0)
+            glVertex3f(x2, y2, 0.0)
+            glTexCoord2f(1.0, 0.0)
+            glVertex3f(x4, y4, 0.0)
+            glTexCoord2f(1.0, 1.0)
+            glVertex3f(x3, y3, 0.0)
+            glTexCoord2f(0.0, 1.0)
+            glVertex3f(x1, y1, 0.0)
 
-        glEnable(GL_TEXTURE_2D)  # 启用2D纹理映射
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(-0.5, -0.5, 0.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(0.5, -0.5, 0.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(0.5, 0.5, 0.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(-0.5, 0.5, 0.0)
-        glEnd()
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0, 0.0)
-        glVertex3f(-1.0, 0.0, 0.0)
-        glTexCoord2f(1.0, 0.0)
-        glVertex3f(0.0, -1.0, 0.0)
-        glTexCoord2f(1.0, 1.0)
-        glVertex3f(1.0, 0.0, 0.0)
-        glTexCoord2f(0.0, 1.0)
-        glVertex3f(0.0, 1.0, 0.0)
         glEnd()
         glDisable(GL_TEXTURE_2D)
         glDisable(GL_ALPHA_TEST)
         glDisable(GL_BLEND)
 
-        glutSwapBuffers()
+
 
 
 if __name__ == '__main__':
-    render = Render(None, None, None)
+    share_list = [{'x':0.0, 'y':10.0,'phi':135.0}]
+    render = Render(share_list, None, None)
